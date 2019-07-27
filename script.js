82e4ea600f93db49f869614cc6cdd905b3236d4c
MarkTangoKilo <mtbouwman@gmail.com>
2019-07-27 14:43:19 -0600

    Massive clean up on the site, most of the stuff is on css and I don't have all of this other crap that's throwing me off. This is the completed base site for the band

--- script.js
+++ script.js
@@ -1,39 +1,46 @@
+var curr_spot;
+var spot_height;
+var spot_width;
 
 $(document).ready(function () {
     $(document).scroll(Scroll);
     Scroll();
+    $("#nav_Home").click(function(){ScrollToHere(0)});
+    $("#nav_About").click(function(){ScrollToHere(1)});
+    $("#nav_Dates").click(function(){ScrollToHere(4)});
+    $("#nav_Music").click(function(){ScrollToHere(5)});
+    $("#nav_Merch").click(function(){ScrollToHere(6)});
 });
 
 function Scroll() {
     curr_spot = $(window).scrollTop();
+    spot_height = $(window).height();
+    spot_width = $(window).width();
     var opacityVal = 0.7 - curr_spot/(spot_height + curr_spot);
-    //if you're below the first div
-    if (curr_spot > spot_height - $("#NextPage_div_container").height()
-    ) {
-        $("#NextPage_div_container").css({
+
+
+    //if you're below the first article
+    if (curr_spot > spot_height - $("#nav_container").height()) {
+        $("#nav_container").css({
             'position': 'fixed',
             'top': '0px',
             'width': spot_width + 'px'
         });
     }
-    //if you're above the first dive
+    //if you're above the first article
     else {
-        $("#NextPage_div_container").css({
+        $("#nav_container").css({
             'width': spot_width + 'px',
             'position': 'absolute',
             'bottom': 0 + '%',
-            'top':spot_height - $("#NextPage_div_container").height() + 'px'
+            'top':spot_height - $("#nav_container").height() + 'px'
         });
     }
-    $("#NextPage_div_throwshade2").css({
-        'opacity': opacityVal
-    });
 }
 
 function ScrollToHere(val){
+    spot_height = $(window).height();
     var iTargetValue = val * spot_height;
-    var iCurrentValue = $(window).scrollTop();
-    var iMoveRate = 0.1;
 
     //$('html, body').animate({scrollTop: iTargetValue}, 1000);
     $('html, body').animate({

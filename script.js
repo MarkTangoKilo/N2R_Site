3618d4a706242c5c2e7439802f71b02aec52d980
MarkTangoKilo <mtbouwman@gmail.com>
2019-07-27 20:27:19 -0600

    Made it so that now the animation will fire whenever the change is made, and I only used css for this, but there is always a width that is the size of a scroll bar on the bottom and I can't find out how to get rid of this stupid shit

--- script.js
+++ script.js
@@ -2,17 +2,25 @@ var curr_spot;
 var spot_height;
 var spot_width;
 var bFixed = new Boolean(false);
+var bDoubleCheck = new Boolean(false);
 
 $(document).ready(function () {
-    $(window).resize(Scroll);
-    $(document).scroll(Scroll);
+    $(document).scroll(CheckScroll);
+    $(window).resize(CheckScroll);
     Scroll();
-    $("#nav_Home").click(function(){ScrollToHere(0)});
-    $("#nav_About").click(function(){ScrollToHere(1)});
-    $("#nav_Dates").click(function(){ScrollToHere(4)});
-    $("#nav_Music").click(function(){ScrollToHere(5)});
-    $("#nav_Merch").click(function(){ScrollToHere(6)});
+    $("#li_Home").click(function(){ScrollToHere(0)});
+    $("#li_About").click(function(){ScrollToHere(1)});
+    $("#li_Dates").click(function(){ScrollToHere(4)});
+    $("#li_Music").click(function(){ScrollToHere(5)});
+    $("#li_Merch").click(function(){ScrollToHere(6)});
 });
+
+function CheckScroll(){
+    Scroll();
+    bDoubleCheck = true;
+    Scroll();
+    bDoubleCheck = false;
+}
 
 function Scroll() {
     curr_spot = $(window).scrollTop();
@@ -21,7 +29,10 @@ function Scroll() {
 
 
     //if you're below the first article
-    if (!bFixed && curr_spot > spot_height - $("#nav_container").height()){
+    if (
+        (!bFixed || bDoubleCheck) 
+        && curr_spot > spot_height - $("#nav_container").height()
+    ){
         $("#nav_container").css({
             'position': 'fixed',
             'top': '0px',
@@ -30,7 +41,10 @@ function Scroll() {
         bFixed = true;
     }
     //if you're above the first article
-    else if (bFixed && curr_spot <= spot_height - $("#nav_container").height()){
+    else if (
+        (bFixed || bDoubleCheck)
+         && curr_spot <= spot_height - $("#nav_container").height()
+    ){
         $("#nav_container").css({
             'position': 'absolute',
             'bottom': 0 + '%',

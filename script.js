a3e61d171b036e07eaeffa31e8915f22f9de5b67
MarkTangoKilo <mtbouwman@gmail.com>
2019-08-07 20:33:46 -0600

    Changed the functions in jquery to split up the onScroll and onResize methods to be separate. Also added different checks in each, where teh scroll only checks if it needs to and the resize checks every time. Leveraged bMobilized and bAlreadyFixed for those tasks (class level bools). The sticky bar still works. As well, I got rid of the animations in CSS, as they were no longer useful for my purposes. I no longer have any animations for the nav bar to switch between mobile and desktop, but I will soon add them to js.

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
+//value in pixels of where the user already is
 var curr_spot;
+//current height in pixels for window
 var spot_height;
+//current width in pixels for window
 var spot_width;
+//true when skinny browser or on mobile device
 var bMobilized = new Boolean(false);
+//true when the bar is fixed at the top
+var bAlreadyFixed = new Boolean(false);
 
 $(document).ready(function () {
     $(document).scroll(CheckScroll);
-    $(window).resize(CheckScroll);
+    $(window).resize(CheckResize);
     CheckScroll();
     $("#li_Home").click(function(){ScrollToHere(0)});
     $("#li_About").click(function(){ScrollToHere(1)});
@@ -14,31 +20,60 @@ $(document).ready(function () {
     $("#li_Contact").click(function(){ScrollToHere(6)});
 });
 
+//checks to see what needs to be done after scrolling
 function CheckScroll(){
-    if($(window).width() <= 675) {
-        bMobilized = true;
-    }
-    else {
-        bMobilized = false;
-    }
-    //finding out what should be done with the nav bar based on the
-    //position of the scrolling
-    if(!bMobilized) {
-        if(curr_spot > spot_height - $("#nav_container").height()) {
-            Scroll(false);
-        }
-        else {
-            Scroll(true);
-        }
-    }
-}
-
-function Scroll(bFixed) {
     curr_spot = $(window).scrollTop();
     spot_height = $(window).height();
     spot_width = $(window).width();
+    ///NAV BAR OR NAV SIDE WINDOW
+    //check to see if I need to get the page into mobile mode
+    if(!bMobilized && $(window).width() <= 675){
+        NavBarMode(true);
+    }
+    //check to see if the page needs to be in desktop mode
+    else if (bMobilized && $(window).width() > 675){
+        NavBarMode(false);
+    }
 
+    ///NAVBAR FIXED OR NAV BAR ABSOLUTE
+    //check to see if we need 
+    if(curr_spot > spot_height - $("#nav_container").height() && !bAlreadyFixed) {
+        NavBarPos(false);
+    }
+    else if(curr_spot <= spot_height - $("#nav_container").height() && bAlreadyFixed){
+        NavBarPos(true);
+    }
+}
 
+//checks to see what needs to be done on a window resize
+function CheckResize(){
+    curr_spot = $(window).scrollTop();
+    spot_height = $(window).height();
+    spot_width = $(window).width();
+    ///NAV BAR OR NAV SIDE WINDOW
+    //check to see if I need to get the page into mobile mode
+    if($(window).width() <= 675)
+    {
+        NavBarMode(true);
+    }
+    //check to see if the page needs to be in desktop mode
+    else if ($(window).width() > 675)
+    {
+        NavBarMode(false);
+    }
+
+    ///NAVBAR FIXED OR NAV BAR ABSOLUTE
+    //check to see if we need 
+    if(curr_spot > spot_height - $("#nav_container").height()) {
+        NavBarPos(false);
+    }
+    else if(curr_spot <= spot_height - $("#nav_container").height()){
+        NavBarPos(true);
+    }
+}
+
+//basically used for sticky nav bar function
+function NavBarPos(bFixed) {
     //if you're below the first article
     if (!bFixed){
         $("#nav_container").css({
@@ -46,7 +81,6 @@ function Scroll(bFixed) {
             'top': '0px',
             'z-index': 100
         });
-        bFixed = true;
     }
     //if you're above the first article
     else if (bFixed){
@@ -56,10 +90,22 @@ function Scroll(bFixed) {
             'top':spot_height - $("#nav_container").height() + 'px',
             'z-index': 100
         });
-        bFixed = false;
     }
+    bAlreadyFixed = !bFixed;
 }
 
+//will switch the bar between mobile mode and desktop mode
+function NavBarMode(bMakeMobile){
+    if(bMakeMobile) {
+
+    }
+    else {
+
+    }
+    bMobilized = !bMakeMobile;
+}
+
+//scrolls to a certain position
 function ScrollToHere(val){
     spot_height = $(window).height();
     var iTargetValue = val * spot_height;
